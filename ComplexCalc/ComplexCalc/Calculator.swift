f62fe86502b8af502c519d9a193a2e90d12f7f4b
aaronluannguyen <aaronluannguyen@gmail.com>
2018-10-05 01:01:54 -0700

    Added functions for make code almost compile. Need to figure out op param type before finishing first step of making code compile successfully

--- ComplexCalc/ComplexCalc/Calculator.swift
+++ ComplexCalc/ComplexCalc/Calculator.swift
@@ -10,4 +10,89 @@ import Foundation
 
 // All your work will go in here
 class Calculator {
+    // Addition for two Int parameters
+    func add(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    // Addition for an array of Ints
+    func add(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    // Addition for two points in paranthesis form
+    func add(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (0, 0)
+    }
+    
+    // Addition for two Points in dictionary form
+    func add(lhs: [String : Int], rhs: [String : Int]) -> [String : Int] {
+        return ["x" : 0, "y" : 0]
+    }
+
+    
+    // Subtraction for two Int parameters
+    func subtract(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    // Subtraction for an array of Ints
+    func subtract(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    // Subtraction for two points in paranthesis form
+    func subtract(lhs: (Int, Int), rhs: (Int, Int)) -> (Int, Int) {
+        return (0, 0)
+    }
+    
+    // Subtraction for two Points in dictionary form
+    func subtract(lhs: [String : Int], rhs: [String : Int]) -> [String : Int] {
+        return ["x" : 0, "y" : 0]
+    }
+    
+    
+    // Multiplication for two Int parameters
+    func multiply(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    // Multiplication for an array of Ints
+    func multiply(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    
+    // Division for two Int parameters
+    func divide(lhs: Int, rhs: Int) -> Int {
+        return 0
+    }
+    
+    // Division for an array of Ints
+    func divide(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    
+    // Count Operation
+    func count(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    
+    // Average Operation
+    func avg(_ args: [Int]) -> Int {
+        return 0
+    }
+    
+    
+    // MathOps operation w/ lhs, rhs, and op
+    func mathOp(lhs: Int, rhs: Int, op: Int) -> Int {
+        return 0
+    }
+    
+    // MathOps operation w/ array of int, beg, and op
+    func mathOp(args: [Int], beg: Int, op: Int) -> Int {
+        return 0
+    }
 }
